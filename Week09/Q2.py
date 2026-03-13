437c20d65bc4975e1999db3eb7b219ab05e76abf
Your Name <Yash.Zinzuvadiya@georgebrown.ca>
2026-03-13 09:45:22 -0400

    fix Q2 threading error

--- Week09/Q2.py
+++ Week09/Q2.py
@@ -17,15 +17,15 @@ def simulate_task(name,duration,lock):
     print(f"DONE {name} {duration}s")
     lock.release()
 
-    def run_threaded (tasks, lock):
-        threads = []
-        for name, duration in tasks:
-            t = threading.Thread(target =simulate_task, args=(name,duration,lock))
-            threads.append(t)
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
+def run_threaded(tasks, lock):
+    threads = []
+    for name, duration in tasks:
+        t = threading.Thread(target=simulate_task, args=(name, duration, lock))
+        threads.append(t)
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()    
 
 # --- Provided below — error handling example from Week 06 ---
 
